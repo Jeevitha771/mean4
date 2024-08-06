eb819d4915b93542cebb17a8f265b896d83a88b3
Jeevitha771 <81794463+Jeevitha771@users.noreply.github.com>
2024-08-07 01:07:41 +0530

    Update karma.conf.js

--- geeks/karma.conf.js
+++ geeks/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+require('dotenv').config();
 module.exports = function (config) {
   process.env.CHROME_BIN = require('puppeteer').executablePath();
 
