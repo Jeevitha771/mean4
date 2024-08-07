a61c872e766ccadc5f5df3136d722d35c9f9f63c
Jeevitha771 <81794463+Jeevitha771@users.noreply.github.com>
2024-08-07 11:00:27 +0530

    Update karma.conf.js

--- geeks/karma.conf.js
+++ geeks/karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -30,11 +29,11 @@ module.exports = function (config) {
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
-    port: 9876,
+    port: process.env.PORT || 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["ChromeHeadless"],
     singleRun: false,
     restartOnFileChange: true,
   });
